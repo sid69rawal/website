d45e92ad436f4faea30f5abdaf0500746961cd83
sid r <sidrawal69@gmail.com>
2025-05-09 07:56:23 +0000

    3:54:43 AM:    ▲ Next.js 15.2.3 3:54:43 AM:    Creating an optimized production build ... 3:54:50 AM: Browserslist: browsers data (caniuse-lite) is 7 months old. Please run: 3:54:50 AM:   npx update-browserslist-db@latest 3:54:50 AM:   Why you should do it regularly: https://github.com/browserslist/update-db#readme 3:54:51 AM: Retrying 1/3... 3:54:51 AM: Retrying 1/3... 3:54:51 AM: Retrying 1/3... 3:54:51 AM: Retrying 1/3... 3:54:54 AM:  ✓ Compiled successfully 3:54:54 AM:    Linting and checking validity of types ... 3:55:00 AM: Failed during stage 'building site': Build script returned non-zero exit code: 2 (https://ntl.fyi/exit-code-2) 3:54:59 AM: Failed to compile. 3:54:59 AM: 3:54:59 AM: ./src/components/ui/resizable.tsx:31:5 3:54:59 AM: Type error: Type '{ children: false | Element | undefined; prefix?: string | undefined; translate?: "yes" | "no" | undefined; content?: string | undefined; popover?: "" | "auto" | "manual" | undefined; ... 276 more ...; className: string; }' is not assignable to type 'IntrinsicAttributes & Omit<HTMLAttributes<keyof HTMLElementTagNameMap>, "id" | "onFocus" | "onBlur"> & { ...; } & { ...; }'. 3:54:59 AM:   Property 'ref' does not exist on type 'IntrinsicAttributes & Omit<HTMLAttributes<keyof HTMLElementTagNameMap>, "id" | "onFocus" | "onBlur"> & { ...; } & { ...; }'. 3:54:59 AM:   29 | >(({ withHandle, className, ...props }, ref) => ( 3:54:59 AM:   30 |   <ResizablePrimitive.PanelResizeHandle 3:54:59 AM: > 31 |     ref={ref} 3:54:59 AM:      |     ^ 3:54:59 AM:   32 |     className={cn( 3:54:59 AM:   33 |       "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90", 3:54:59 AM:   34 |       className 3:54:59 AM: Next.js build worker exited with code: 1 and signal: null 3:54:59 AM: ​ 3:54:59 AM: "build.command" failed 3:54:59 AM: ──────────────────────────────────────────────────────────────── 3:54:59 AM: ​ 3:54:59 AM:   Error message 3:54:59 AM:   Command failed with exit code 1: npm run build (https://ntl.fyi/exit-code-1) 3:54:59 AM: ​ 3:54:59 AM:   Error location 3:54:59 AM:   In Build command from Netlify app: 3:54:59 AM:   npm run build 3:54:59 AM: ​ 3:54:59 AM:   Resolved config 3:54:59 AM:   build: 3:54:59 AM:     command: npm run build 3:54:59 AM:     commandOrigin: ui 3:54:59 AM:     publish: /opt/build/repo/.next 3:54:59 AM:     publishOrigin: ui 3:54:59 AM:   plugins: 3:54:59 AM:     - inputs: {} 3:54:59 AM:       origin: ui 3:54:59 AM:       package: '@netlify/plugin-nextjs' 3:55:00 AM: Build failed due to a user error: Build script returned non-zero exit code: 2 3:55:00 AM: Failing build: Failed to build site 3:55:00 AM: Finished processing build request in 47.205s

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import * as React from "react" // Added React import
+import * as React from "react"
 import { GripVertical } from "lucide-react"
 import * as ResizablePrimitive from "react-resizable-panels"
 
@@ -21,14 +21,14 @@ const ResizablePanelGroup = ({
 
 const ResizablePanel = ResizablePrimitive.Panel
 
-const ResizableHandle = React.forwardRef< 
+const ResizableHandle = React.forwardRef<
   React.ElementRef<typeof ResizablePrimitive.PanelResizeHandle>,
-  React.ComponentProps<typeof ResizablePrimitive.PanelResizeHandle> & {
+  React.ComponentPropsWithoutRef<typeof ResizablePrimitive.PanelResizeHandle> & {
     withHandle?: boolean
   }
->(({ withHandle, className, ...props }, ref) => ( 
+>(({ withHandle, className, ...props }, ref) => (
   <ResizablePrimitive.PanelResizeHandle
-    ref={ref} 
+    ref={ref}
     className={cn(
       "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
       className
@@ -42,6 +42,6 @@ const ResizableHandle = React.forwardRef<
     )}
   </ResizablePrimitive.PanelResizeHandle>
 ))
-ResizableHandle.displayName = "ResizableHandle" 
+ResizableHandle.displayName = "ResizableHandle"
 
 export { ResizablePanelGroup, ResizablePanel, ResizableHandle }
