a1b99bc9ececf1bd404f943cb2dcf19c9336c821
sid r <sidrawal69@gmail.com>
2025-05-10 17:20:19 +0000

    Fix

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -30,14 +30,16 @@ export default function ThankYouPage() {
               </CardTitle>
             </CardHeader>
             <CardContent>
-              <CardDescription className="text-lg text-muted-foreground mb-8">
-                {siteConfig.thankYouPage.description}
-              </CardDescription>
-              <Button asChild className="w-full max-w-xs mx-auto btn-effect"> {/* Added btn-effect */}
-                <Link href={siteConfig.thankYouPage.buttonLink}>
-                  {siteConfig.thankYouPage.buttonText}
-                </Link>
-              </Button>
+              <React.Fragment>
+                <CardDescription className="text-lg text-muted-foreground mb-8">
+                  {siteConfig.thankYouPage.description}
+                </CardDescription>
+                <Button asChild className="w-full max-w-xs mx-auto btn-effect"> {/* Added btn-effect */}
+                  <Link href={siteConfig.thankYouPage.buttonLink}>
+                    {siteConfig.thankYouPage.buttonText}
+                  </Link>
+                </Button>
+              </React.Fragment>
             </CardContent>
           </Card>
         </div>
